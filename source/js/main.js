df1d82322bfffbe61854bc04433c73ab6a1229d9
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-12-14 21:40:27 +0300

    feat: add menu toggle fix: make changes after testing the project

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,12 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+// header меню
+const headerNavigation = document.querySelector('.header__navigation');
+const headerButton = document.querySelector('.header__toggle');
+
+headerButton.addEventListener('click', () => {
+  headerNavigation.classList.toggle('header__navigation--opened');
+})
