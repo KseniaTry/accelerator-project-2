33118f74ca51e9549274790caf121aaa8fbcd681
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-12-17 12:09:11 +0300

    feat: add styles in hero block (mobile, tablet, desktop)

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,45 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import 'swiper/css';
 
 
 // header меню
 const headerNavigation = document.querySelector('.header__navigation');
 const headerButton = document.querySelector('.header__toggle');
+const navigationItems = document.querySelectorAll('.navigation__item');
 
 headerButton.addEventListener('click', () => {
   headerNavigation.classList.toggle('header__navigation--opened');
 })
+
+navigationItems.forEach((navigationItem) => {
+  navigationItem.addEventListener('click', () => {
+    headerNavigation.classList.remove('header__navigation--opened');
+  })
+})
+
+// hero swiper slider
+const heroSlider = document.querySelector('.hero__swiper');
+
+new Swiper(heroSlider, {
+  slideClass: 'hero__slide',
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  loop: true,
+  centeredSlides: true,
+  pagination: {
+    el: '.hero__button',
+    type: 'bullets',
+    bulletClass: 'swiper-pagination-bullet hero__bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active hero__bullet--active',
+  },
+  breakpoints: {
+    1400: {
+      allowTouchMove: false,
+      pagination: {
+        clickable: true,
+      }
+    },
+  }
+});
